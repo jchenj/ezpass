873429d736de689db063ddd7c64b1ac30937210d
Julianna Chen <c.julianna@gmail.com>
2020-03-28 10:24:20 +0800

    Work in progress on Issue #14

--- getpass.py
+++ getpass.py
@@ -20,23 +20,57 @@ def get_password_from_file(account):
 
     with open(DATAFILE, encoding='utf-8-sig') as csvfile:
         reader = csv.DictReader(csvfile)
-        found = False
         for row in reader:
             for k, v in row.items():
+                # print(k, v)
                 if k == account_header and v.strip() == account:  # Assumes headers are free of extra spaces
-                    found = True
                     password = row[password_header].strip()
                     pyperclip.copy(password)
                     print("Password for account '{}' in paste buffer".format(account))
-        if not found:
-            raise RuntimeError("Account '{}' not in file".format(account))
+                    return
+    raise RuntimeError("Account '{}' not in file".format(account))
+
+
+def check_if_account_exists(account):
+    account_header = 'Account-name'
+    password_header = 'Password'
+
+    with open(DATAFILE, encoding='utf-8-sig') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            for k, v in row.items():
+                # print(k, v)
+                if k == account_header and v.strip() == account:  # Assumes headers are free of extra spaces
+                    return True
+        return False
+
+
+def create_new_account(account):
+    if check_if_account_exists(account):
+        raise RuntimeError("Account '{}' already exists".format(account))
+    print("Creating new account with", account)
+
+# generate password
+# open file for writing
+# add new record for file at the end of file
+# close & save file
+
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Retrieve password.')
-    parser.add_argument('account', type=str, help='account name')
+    parser.add_argument('--get-pass', type=str, help='account name')
+    parser.add_argument('--new-account', type=str, help='new account name')
     args = parser.parse_args()
+
+    print(args)
     try:
-        get_password_from_file(args.account)
+        if args.get_pass is not None and args.new_account is not None:
+            # both are set: Error
+            print("Error. Can't get password & create new account at same time")
+        elif args.get_pass is not None:
+            get_password_from_file(args.get_pass)
+        elif args.new_account is not None:
+            create_new_account(args.new_account)
     except RuntimeError as err:
         print(err)
