b9cc1509827a8bc36d3cbb6e70405d6b4ba7fb89
Julianna Chen <c.julianna@gmail.com>
2020-04-02 13:37:12 +0800

    closes issue #24

--- test_passman.py
+++ test_passman.py
@@ -1,41 +1,41 @@
-import getpass
+import passman
 import unittest
 import pyperclip
 import random
 
 fname = 'test-file.csv'
-password_length = 2 * len(getpass.ALPHABET)
+password_length = 2 * len(passman.ALPHABET)
 
 
 class Tests(unittest.TestCase):
 
     def test_generate_random_letter(self):
-        letter = getpass.generate_random_letter('abcd')
+        letter = passman.generate_random_letter('abcd')
         self.assertEqual(len(letter), 1)
         self.assertIn(letter, 'abcd')
 
     def test_check_if_account_exists(self):
         account = 'bird'
         # existing account
-        ret = getpass.check_if_account_exists(fname, account)
+        ret = passman.check_if_account_exists(fname, account)
         self.assertEqual(ret, True)
         # non-existing account
         account = 'pig'
-        ret = getpass.check_if_account_exists(fname, account)
+        ret = passman.check_if_account_exists(fname, account)
         self.assertEqual(ret, False)
 
     def test_get_password_from_file(self):
         account = 'bird'
         password = "feathers"
         # valid account - default
-        getpass.get_password_from_file(fname, account, False)
+        passman.get_password_from_file(fname, account, False)
         pw = pyperclip.paste()
         self.assertEqual(pw, password)
         # not testing valid account - print screen
         account = 'pig'
         # invalid account - default
         try:
-            getpass.get_password_from_file(fname, account, False)
+            passman.get_password_from_file(fname, account, False)
             self.fail("Did not raise expected exception")
         except RuntimeError as err:
             pass
@@ -43,11 +43,11 @@ class Tests(unittest.TestCase):
         # not testing invalid account - print to screen
 
     def test_create_password(self):
-        length = random.randint(1, (2 * len(getpass.ALPHABET)))
-        password = getpass.create_password(length)
+        length = random.randint(1, (2 * len(passman.ALPHABET)))
+        password = passman.create_password(length)
         self.assertEqual(len(password), length)
         for letter in password:
-            self.assertIn(letter, getpass.ALPHABET)
+            self.assertIn(letter, passman.ALPHABET)
 
     def create_random_account(self):
         #! TODO: create static func from this method?
@@ -56,13 +56,13 @@ class Tests(unittest.TestCase):
         # create a random string of specified length using letters in alphabet
         random_account = ""
         for i in range(length):
-            letter = getpass.generate_random_letter(getpass.ALPHABET)
+            letter = passman.generate_random_letter(passman.ALPHABET)
             random_account = random_account + letter
         return random_account
 
     def get_non_existing_account(self):
         random_account = self.create_random_account()
-        if getpass.check_if_account_exists(fname, random_account):
+        if passman.check_if_account_exists(fname, random_account):
             return self.get_non_existing_account()
         else:
             return random_account
@@ -71,32 +71,32 @@ class Tests(unittest.TestCase):
         # account already exists in file
         account = "dog"
         try:
-            getpass.create_new_account(fname, account, password_length)
+            passman.create_new_account(fname, account, password_length)
             self.fail("Did not raise expected exception")
         except RuntimeError as err:
             pass
         # any unexpected exceptions will be caught by unittest framework
         # account doesn't yet exist in file
         account = self.get_non_existing_account()
-        ret = getpass.check_if_account_exists(fname, account)
+        ret = passman.check_if_account_exists(fname, account)
         self.assertEqual(ret, False)
-        getpass.create_new_account(fname, account, password_length)
-        ret = getpass.check_if_account_exists(fname, account)
+        passman.create_new_account(fname, account, password_length)
+        ret = passman.check_if_account_exists(fname, account)
         self.assertEqual(ret, True)
 
     def test_delete_account_existing(self):
         # account exists
         account = self.get_non_existing_account()
-        getpass.create_new_account(fname, account, password_length)
-        getpass.delete_account(fname, account)
-        ret = getpass.check_if_account_exists(fname, account)
+        passman.create_new_account(fname, account, password_length)
+        passman.delete_account(fname, account)
+        ret = passman.check_if_account_exists(fname, account)
         self.assertEqual(ret, False)
 
     def test_delete_account_non_existing(self):
         # account doesn't exist
         account = self.get_non_existing_account()
         try:
-            getpass.delete_account(fname, account)
+            passman.delete_account(fname, account)
             self.fail("Did not raise expected error")
         except RuntimeError as err:
             pass
