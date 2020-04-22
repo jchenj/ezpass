f08bb78cc8609115596c93c9c8d4734fee910ff7
Julianna Chen <c.julianna@gmail.com>
2020-04-22 15:59:16 +0800

    addresses issue #16

--- tests.sh
+++ tests.sh
@@ -2,9 +2,22 @@
 
   echo "Running tests..."
 
+# Create new file
   python3 passman.py -f testfile -nf
+# Create new account
   python3 passman.py -f testfile -na bob
-  python3 passman.py -f testfile -cp bob
+# Get password for account
+  python3 passman.py -f testfile -g bob
+# Change password, specifying password length
+#! TODO: check below - how do I know if it's creating a PW of correct length?
+  python3 passman.py -f testfile -cp bob -l 4
+# Change password with specified password
+#! TODO: check below - how do I know if it's creating correct PW?
+  python3 passman.py -f testfile -cp bob -sp 8080boat
+# Get password, printing to screen
+  python3 passman.py -f testfile -g bob -print
+#! Delete account
+  python3 passman.py -f testfile -d bob
 
   rm testfile
 
